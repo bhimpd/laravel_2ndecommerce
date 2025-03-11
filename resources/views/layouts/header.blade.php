dcc03cb09924507bc0a271c5c8a5b802dfbb68a5
intuji-bhimlamichhane <bhim.lamichhane@intuji.com>
2025-03-11 18:11:15 +0545

    minor fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ecommerce</title>
+    <title>Home Page</title>
 
     {{-- bootstrap link --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -30,15 +30,17 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <!-- Search Bar Centered -->
                 <form class="d-flex mx-auto w-50">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                    <div class="input-group">
+                        <input class="form-control" type="search" placeholder="Search Products" aria-label="Search">
+                        <button class="btn btn-outline-primary btn-sm" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
                 </form>
     
                 <!-- Right-side items -->
                 <div class="d-flex align-items-center gap-3 ms-auto">
                     <a href="#" class="text-decoration-none text-dark fw-semibold">Become a Seller</a>
                     <a href="#" class="btn btn-outline-success btn-sm"><i class="fa-solid fa-cart-shopping  me-2"></i>Cart</a>
-                    <a href="#" class="btn btn-primary btn-sm"><i class="fa-solid fa-user  me-2"></i>Login</a>
+                    <a href="#" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-user  me-2"></i>Login</a>
                 </div>
             </div>
         </div>
