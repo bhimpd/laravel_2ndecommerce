ebb37092a6bf6e585971863d3f759a3d2f070f1a
intuji-bhimlamichhane <bhim.lamichhane@intuji.com>
2025-03-11 20:32:23 +0545

    added css for header footer and categories

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 
     {{-- footerpart     --}}
-    <footer class="text-center p-3 bg-light">
+    <footer class="text-center p-3 footer">
         © 2025 Ecommerce. All Rights Reserved. Designed and coded by vim v@i
     </footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,9 +19,9 @@
   <body>
 
     {{-- headerpart --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm nav-header">
         <div class="container">
-            <a class="navbar-brand fw-bold text-primary" href="#">Ecommerce</a>
+            <a class="navbar-brand fw-bold" href="#">Ecommerce</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -38,7 +38,7 @@
     
                 <!-- Right-side items -->
                 <div class="d-flex align-items-center gap-3 ms-auto">
-                    <a href="#" class="text-decoration-none text-dark fw-semibold">Become a Seller</a>
+                    <a href="#" class="text-decoration-none fw-semibold">Become a Seller</a>
                     <a href="#" class="btn btn-outline-success btn-sm"><i class="fa-solid fa-cart-shopping  me-2"></i>Cart</a>
                     <a href="#" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-user  me-2"></i>Login</a>
                 </div>
@@ -46,6 +46,34 @@
         </div>
     </nav>
 
+    {{-- categories list   --}}
+
+    <nav class="navbar navbar-expand-lg categories">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Mobile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Fashion</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Electronics</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Furniture</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Grocery</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Appliances</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
