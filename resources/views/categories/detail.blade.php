da7e4c9a0dbd4d853f83cfc5e8212f1af3e3d7f0
intuji-bhimlamichhane <bhim.lamichhane@intuji.com>
2025-03-13 21:49:27 +0545

    added paroduct details page

--- resources/views/categories/detail.blade.php
+++ resources/views/categories/detail.blade.php
@@ -24,11 +24,22 @@
                     <span class="fa fa-star checked"></span>
                     <span class="fa fa-star checked"></span>
                     <span class="fa fa-star"></span>
-                    <h6>Customer Rating</h6>
                 </div>
                 <p>
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                 </p>
+                <div>
+                    <div class="d-flex flex-row mb-3">
+                        <div class="p-2"><h5>Quantity</h5></div>
+                        <div class="p-2">
+                            <span class="btn btn-primary btn-sm rounded-start-pill"><i class="fa-solid fa-minus"></i></span>
+                            <span class="mx-2">01</span>
+                            <span class="btn btn-primary btn-sm rounded-end-pill"><i class="fa-solid fa-plus"></i></span>
+
+                        </div>
+                       
+                      </div>
+                </div>
                 <div>
                     <a href="" class="btn theme-green-btn text-light rounded-pill me-1">Add to Cart</a>
                     <a href="" class="btn theme-orange-btn text-light rounded-pill me-1">Buy Now</a>
@@ -175,10 +186,42 @@
 
             {{-- Add Review --}}
             <section>
-                <div class="container">
+                <div class="container mt-4 bg-light p-5">
+                    <h2>Add a Review</h2>
                     <div class="row">
                         <div class="col-lg-12">
+                            <form action="">
+                                <div id="rateproduct" class="form-text">Rate this Product
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                </div>
 
+                                <div class="row my-2">
+                                        
+                                    <div class="col-lg-6 mb-3">
+                                        <input type="text" class="form-control form-control-lg" id="name" placeholder="Name Here">
+
+                                    </div>
+                                    <div class="col-lg-6 mb-3">
+                                        <input type="email" class="form-control form-control-lg" id="email" placeholder="Email Here">
+
+                                    </div>
+
+                                    <div class="col-lg-12 mb-3">
+                                        <textarea class="form-control form-control-lg" id="message" placeholder="Your Message" rows="3"></textarea>
+
+                                    </div>
+
+                                    <div>
+                                        <a href="" class="btn theme-orange-btn text-light rounded-pill me-1">Post a comment</a>
+                                    </div>
+                                    
+                                        
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
